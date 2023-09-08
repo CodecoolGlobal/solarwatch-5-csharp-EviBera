0d548d9119623b15d7d616e6d15b58c3fddabe2f
Évi <beraevi@gmail.com>
2023-09-08 15:56:16 +0200

    unit testing the controller

--- SolarWatch5Test/CityControllerTests.cs
+++ SolarWatch5Test/CityControllerTests.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using SolarWatch5.Controllers;
+using SolarWatch5.Models;
 using SolarWatch5.Services;
+using SolarWatch5.Services.Repository;
 
-namespace SolarWatch3DockerTest
+namespace SolarWatch5Test
 {
     [TestFixture]
     public class CityControllerTests
@@ -12,6 +14,8 @@ namespace SolarWatch3DockerTest
         private Mock<ILogger<CityController>> _loggerMock;
         private Mock<ICityService> _cityServiceMock;
         private Mock<IJsonProcessor> _jsonProcessorMock;
+        private Mock<ICityRepository> _cityRepositoryMock;
+        private Mock<ISolarDataRepository> _sunsetSunriseDataRepositoryMock;
         private CityController _controller;
 
         [SetUp]
@@ -20,74 +24,78 @@ namespace SolarWatch3DockerTest
             _loggerMock = new Mock<ILogger<CityController>>();
             _cityServiceMock = new Mock<ICityService>();
             _jsonProcessorMock = new Mock<IJsonProcessor>();
-            _controller = new CityController(_loggerMock.Object, _cityServiceMock.Object);
+            _cityRepositoryMock = new Mock<ICityRepository>();
+            _sunsetSunriseDataRepositoryMock = new Mock<ISolarDataRepository>();
+            _controller = new CityController(_loggerMock.Object, _cityServiceMock.Object, _jsonProcessorMock.Object,
+                _cityRepositoryMock.Object, _sunsetSunriseDataRepositoryMock.Object);
         }
 
         [Test]
-        public async Task GetReturnsNotFoundResultIfCityServiceFails()
+        public async Task GetAsync_WhenCityExistsAndSolarDataAvailable_ShouldReturnOkResult()
         {
-            //arrange
-            _cityServiceMock.Setup(item => item.GetCityAsync(It.IsAny<string>(), It.IsAny<DateOnly>()))
-              .Throws(new Exception());
+            // Arrange
+            var cityName = "TestCity";
+            var day = new DateOnly(2023, 9, 7);
 
-            //act
-            var result = await _controller.GetAsync("Budapest", DateOnly.FromDateTime(DateTime.Now));
+            var city = new City() { Id = 100, CityName = "TestCity", Lat = 33.33, Lon = 33.33, Country = "TestCountry"};
+            var solarData = new SunsetSunriseData { Id = 100, Date = new DateTime(2023, 9, 7), 
+                Sunrise = new DateTime(2023, 9, 7, 6, 0, 0), Sunset = new DateTime(2023, 9, 7, 19, 30, 0), CityId = 100 };
 
-            //assert
-            Assert.That(result.Result, Is.InstanceOf<NotFoundObjectResult>());
+            _cityRepositoryMock.Setup(repo => repo.GetByNameAsync(cityName))
+                .ReturnsAsync(city);
+            _sunsetSunriseDataRepositoryMock.Setup(repo => repo.GetByDateAndCityAsync(day, city.Id))
+                .ReturnsAsync(solarData);
+
+            // Act
+            var result = await _controller.GetAsync(cityName, day);
+
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+            var okResult = (OkObjectResult)result.Result;
+            Assert.IsInstanceOf<CityWithSolarData>(okResult.Value);
         }
 
         [Test]
-        public async Task GetReturnsOkIfCityServiceWorks()
+        public async Task GetAsyncReturnsNotFoundResult_IfCityIsNotAvailable()
         {
-            //arrange
-            var expectedCity = new SolarWatch5.Models.City();
-            _cityServiceMock.Setup(item => item.GetCityAsync(It.IsAny<string>(), It.IsAny<DateOnly>()))
-                .ReturnsAsync(expectedCity);
 
-            //act
-            var result = await _controller.GetAsync("Budapest", DateOnly.FromDateTime(DateTime.Now));
+            var cityName = "NonExistentCity";
+            
+            _cityRepositoryMock.Setup(repo => repo.GetByNameAsync(cityName))
+                .ReturnsAsync((City)null);
+            
+            // Act
+            var result = await _controller.GetAsync(cityName, new DateOnly(2023, 9, 7));
 
-            //assert
-            Assert.IsInstanceOf(typeof(OkObjectResult), result.Result);
-            Assert.That(((OkObjectResult)result.Result).Value, Is.EqualTo(expectedCity));
+            // Assert
+            Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
+
         }
+        
 
         [Test]
-        public async Task GetReturnsValidCityIfCityServiceWorks()
+        public async Task GetAsyncReturnsNotFoundResult_IfSolarDataAreNotAvailable()
         {
-            DateOnly day = DateOnly.FromDateTime(DateTime.Now);
-            SolarWatch5.Models.City mockCity = new SolarWatch5.Models.City()
-            {
-                CityName = "London"
-            };
 
-            _cityServiceMock.Setup(item => item.GetCityAsync("London", day))
-                .ReturnsAsync(mockCity);
+            var cityName = "TestCity";
+            var day = new DateOnly(2023, 9, 7);
 
-            var result = await _controller.GetAsync("London", day);
+            var city = new City() { Id = 100, CityName = "TestCity", Lat = 33.33, Lon = 33.33, Country = "TestCountry" };
+            SunsetSunriseData solarData = null;
 
-            Assert.IsInstanceOf(typeof(OkObjectResult), result.Result);
-            Assert.That(((OkObjectResult)result.Result).Value, Is.EqualTo(mockCity));
+            _cityRepositoryMock.Setup(repo => repo.GetByNameAsync(cityName))
+                .ReturnsAsync(city);
+            _sunsetSunriseDataRepositoryMock.Setup(repo => repo.GetByDateAndCityAsync(day, city.Id))
+                .ReturnsAsync(solarData);
+
+            // Act
+            var result = await _controller.GetAsync(cityName, day);
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
 
         }
 
-        [Test]
-        public async Task GetReturnsTheGivenCityIfCityServiceWorks()
-        {
-            DateOnly day = DateOnly.FromDateTime(DateTime.Now);
-            SolarWatch5.Models.City mockCity = new SolarWatch5.Models.City()
-            {
-                CityName = "London"
-            };
-
-            _cityServiceMock.Setup(item => item.GetCityAsync("London", day))
-                .ReturnsAsync(mockCity);
-
-            var result = await _controller.GetAsync("London", day);
-
-            Assert.IsInstanceOf(typeof(OkObjectResult), result.Result);
-            Assert.That(((SolarWatch5.Models.City)((OkObjectResult)(result.Result)).Value).CityName, Is.EqualTo("London"));
-        }
+        
     }
 }
